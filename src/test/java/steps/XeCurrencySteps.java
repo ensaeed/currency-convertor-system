8c631332dfb4066eac8902acd35d2e7d1e2dbac5
Ehsin <ensaeed@yahoo.co.uk>
2026-04-27 00:37:33 +0100

    Fix CI: surefire + playwright setup

--- src/test/java/steps/XeCurrencySteps.java
+++ src/test/java/steps/XeCurrencySteps.java
@@ -23,7 +23,7 @@ public class XeCurrencySteps {
         playwright = Playwright.create();
 
         browser = playwright.chromium().launch(
-                new BrowserType.LaunchOptions().setHeadless(false)
+                new BrowserType.LaunchOptions().setHeadless(true)
         );
 
         page = browser.newPage();
